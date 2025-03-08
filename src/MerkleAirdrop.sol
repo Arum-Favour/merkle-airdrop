4dd77bb7d0c3526cce7816f62a9e4862a01cca81
Arum <nwajiudo@gmail.com>
2025-03-08 19:16:35 +0100

    feat: enhance MerkleAirdrop with ERC-20 token support and implement WegelToken minting functionality

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -2,7 +2,17 @@
 
 pragma solidity ^0.8.24;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract MerkleAirdrop {
     //list of addresses
-    //allow someone in the list to create tokens
+    //allow someone in the list to create ERC-20 tokens
+    address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
 }
--- src/WegelToken.sol
+++ src/WegelToken.sol
@@ -5,4 +5,10 @@ pragma solidity ^0.8.24;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WegelToken {}
+contract WegelToken is ERC20, Ownable(msg.sender) {
+    constructor() ERC20("Wegel", "WGL") {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
