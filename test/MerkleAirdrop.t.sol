841d1e18b9e737340d075c944c1535221a2d26bb
Arum <nwajiudo@gmail.com>
2025-03-10 23:03:28 +0100

    feat: add Merkle proof generation scripts and input/output JSON handling

---
+++ test/MerkleAirdrop.t.sol
@@ -0,0 +1,11 @@
+// // SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "src/MerkleAirdrop.sol";
+import {WegelToken} from "../src/WegelToken.sol";
+
+contract MerkleAirdropTest is Test {
+    function testUsersCanClaim() public {}
+}
