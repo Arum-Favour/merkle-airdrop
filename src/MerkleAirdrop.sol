923725f04bfdadfd0fc14fb4742da18ba3b0c207
Arum <nwajiudo@gmail.com>
2025-04-11 23:34:23 +0100

    added signature verification

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.24;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
-contract MerkleAirdrop {
+contract MerkleAirdrop is EIP712 {
     using SafeERC20 for IERC20;
 
     error MerkleAirdrop_InvalidProof();
@@ -20,9 +21,20 @@ contract MerkleAirdrop {
 
     mapping(address claimer => bool) private s_hasClaimed;
 
+    bytes32 private constant MESSAGE_TYPEHASH =
+        keccak256("AirdropClaim(address account,uint256 amount)");
+
+    struct AirdropClaim {
+        address account;
+        uint256 amount;
+    }
+
     event Claim(address account, uint256 amount);
 
-    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+    constructor(
+        bytes32 merkleRoot,
+        IERC20 airdropToken
+    ) EIP712("MerkleAirdrop", "1") {
         i_merkleRoot = merkleRoot;
         i_airdropToken = airdropToken;
     }
@@ -39,9 +51,7 @@ contract MerkleAirdrop {
             revert MerkleAirdrop_alreadyClaimed();
         }
         //check the signature
-        if (
-            !_isValidSignature(account, getMessage(account, message), v, r, s)
-        ) {
+        if (!_isValidSignature(account, getMessage(account, amount), v, r, s)) {
             revert MerkleAirdrop_InvalidSignature();
         }
         //calculate the hash of the account usng the leaf node
@@ -54,6 +64,21 @@ contract MerkleAirdrop {
         s_hasClaimed[account] = true;
         emit Claim(account, amount);
         i_airdropToken.safeTransfer(account, amount);
+    }
+
+    function getMessage(
+        address account,
+        uint256 amount
+    ) public view returns (bytes32) {
+        return
+            _hashTypedDataV4(
+                keccak256(
+                    abi.encode(
+                        MESSAGE_TYPEHASH,
+                        AirdropClaim({account: account, amount: amount})
+                    )
+                )
+            );
     }
 
     function getMessage(
